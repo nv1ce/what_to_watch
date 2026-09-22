c9a0b76208f2f0967b9fa5aa5f2345e2d1612e53
Nikita Novikov <nknovikoff@gmail.com>
2026-09-22 17:44:48 +0300

    add SQLAlchemy

---
+++ .env.example
@@ -0,0 +1,2 @@
+FLASK_APP=opinions_app
+FLASK_DEBUG=1
--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+# Импортировать класс для работы с ORM.
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Подключить БД SQLite.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# Создать экземпляр SQLAlchemy и в качестве параметра
+# передать в него экземпляр приложения Flask.
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index_view():
     return 'Совсем скоро тут будет случайное мнение о фильме!'
+
 
 if __name__ == '__main__':
     app.run()
