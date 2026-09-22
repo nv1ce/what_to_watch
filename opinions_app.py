5b21e37275883cd5a18f1a493fe796a519225ed2
Nikita Novikov <nknovikoff@gmail.com>
2026-09-22 18:59:03 +0300

    add db

--- opinions_app.py
+++ opinions_app.py
@@ -1,19 +1,36 @@
+from datetime import datetime
+
+from random import randrange
+
 from flask import Flask
-# Импортировать класс для работы с ORM.
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-# Подключить БД SQLite.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-# Создать экземпляр SQLAlchemy и в качестве параметра
-# передать в него экземпляр приложения Flask.
 db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    # Определить количество мнений в базе данных.
+    quantity = Opinion.query.count()
+    # Если мнений нет...
+    if not quantity:
+        # ...то вернуть сообщение:
+        return 'В базе данных мнений о фильмах нет.'
+    # Иначе выбрать случайное число в диапазоне от 0 до quantity...
+    offset_value = randrange(quantity)
+    # ...и определить случайный объект.
+    opinion = Opinion.query.offset(offset_value).first()
+    return opinion.text
 
 
 if __name__ == '__main__':
