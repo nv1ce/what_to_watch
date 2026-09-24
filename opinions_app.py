2796e895c536ebb2fc4b22409d67a9e18006e08c
Nikita Novikov <nknovikoff@gmail.com>
2026-09-24 18:25:28 +0300

    add custom command load_opinions

--- opinions_app.py
+++ opinions_app.py
@@ -1,3 +1,6 @@
+import csv
+
+import click
 from datetime import datetime
 from random import randrange
 
@@ -102,6 +105,27 @@ def page_not_found(error):
     # При ошибке 404 в качестве ответа вернётся страница, созданная
     # на основе шаблона 404.html и код HTTP-ответа 404.
     return render_template('404.html'), 404
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открыть файл.
+    with open('opinions.csv', encoding='utf-8') as f:
+        # Создать итерируемый объект, который отображает каждую строку
+        # в качестве словаря с ключами из шапки файла.
+        reader = csv.DictReader(f)
+        # Для подсчёта строк добавить счётчик.
+        counter = 0
+        for row in reader:
+            # Распакованный словарь использовать
+            # для создания экземпляра модели Opinion.
+            opinion = Opinion(**row)
+            # Добавить объект в сессию и закоммитить
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 if __name__ == '__main__':
