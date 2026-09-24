f2267362b1a1dc847b54e6b8cbd82715ac3ca312
Nikita Novikov <nknovikoff@gmail.com>
2026-09-24 18:27:31 +0300

    update app file

--- opinions_app.py
+++ opinions_app.py
@@ -1,24 +1,22 @@
 import csv
-
-import click
 from datetime import datetime
 from random import randrange
 
+import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SECRET_KEY'] = 'MY SECRET KEY'
+app.config['SECRET_KEY'] = 'SECRET KEY'
 
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 
@@ -34,15 +32,16 @@ class Opinion(db.Model):
 class OpinionForm(FlaskForm):
     title = StringField(
         'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'), Length(1, 128)],
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
     )
     text = TextAreaField(
         'Напишите мнение',
-        validators=[DataRequired(message='Обязательное поле')],
+        validators=[DataRequired(message='Обязательное поле')]
     )
     source = URLField(
         'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()],
+        validators=[Length(1, 256), Optional()]
     )
     submit = SubmitField('Добавить')
 
@@ -51,8 +50,6 @@ class OpinionForm(FlaskForm):
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        # Если в базе пусто - при запросе к главной странице
-        # пользователь увидит ошибку 500.
         abort(500)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
@@ -64,11 +61,8 @@ def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
         text = form.text.data
-        # Если в БД уже есть мнение с текстом, который ввёл пользователь...
         if Opinion.query.filter_by(text=text).first() is not None:
-            # ...вызвать функцию flash и передать соответствующее сообщение.
             flash('Такое мнение уже было оставлено ранее!')
-            # Вернуть пользователя на страницу «Добавить новое мнение».
             return render_template('add_opinion.html', form=form)
         opinion = Opinion(
             title=form.title.data,
@@ -81,47 +75,31 @@ def add_opinion_view():
     return render_template('add_opinion.html', form=form)
 
 
-@app.route('/opinions/<int:id>')
+@app.route('/opinion/<int:id>')
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
 
 
-# Тут декорируется обработчик и указывается код нужной ошибки.
-@app.errorhandler(500)
-def internal_error(error):
-    # Ошибка 500 возникает в нештатных ситуациях на сервере.
-    # Например, провалилась валидация данных.
-    # В таких случаях можно откатить изменения, незафиксированные в БД,
-    # чтобы в базу не записалось ничего лишнего.
-    db.session.rollback()
-    # Пользователю вернётся страница,
-    # сгенерированная на основе шаблона 500.html и код HTTP-ответа 500.
-    return render_template('500.html'), 500
-
-
 @app.errorhandler(404)
 def page_not_found(error):
-    # При ошибке 404 в качестве ответа вернётся страница, созданная
-    # на основе шаблона 404.html и код HTTP-ответа 404.
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 @app.cli.command('load_opinions')
 def load_opinions_command():
     """Функция загрузки мнений в базу данных."""
-    # Открыть файл.
     with open('opinions.csv', encoding='utf-8') as f:
-        # Создать итерируемый объект, который отображает каждую строку
-        # в качестве словаря с ключами из шапки файла.
         reader = csv.DictReader(f)
-        # Для подсчёта строк добавить счётчик.
         counter = 0
         for row in reader:
-            # Распакованный словарь использовать
-            # для создания экземпляра модели Opinion.
             opinion = Opinion(**row)
-            # Добавить объект в сессию и закоммитить
             db.session.add(opinion)
             db.session.commit()
             counter += 1
